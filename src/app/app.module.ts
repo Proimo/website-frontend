e74368c3bdd28db6a28e05ebc4130255ed83f1bc
Dan Percic <dan.percic@student.upt.ro>
2020-10-06 00:01:07 +0300

    add banner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,24 @@ import {HomePageComponent} from './pages/home-page/home-page.component';
 import { MenuComponent } from './components/menu/menu.component';
 import {HttpClientModule} from '@angular/common/http';
 import {SharedModule} from './shared/shared.module';
+import { BannerComponent } from './pages/home-page/components/banner/banner.component';
+import { SearchBarComponent } from './pages/home-page/components/search-bar/search-bar.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomePageComponent,
-        MenuComponent
+        MenuComponent,
+        BannerComponent,
+        SearchBarComponent
     ],
     imports: [
         BrowserModule.withServerTransition({appId: 'serverApp'}),
         AppRoutingModule,
         HttpClientModule,
-        SharedModule
+        SharedModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
