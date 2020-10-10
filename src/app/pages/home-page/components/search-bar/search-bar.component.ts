69895aceb7f3eb17d35208c31d8137b51c340b0a
Dan Percic <dan.percic@student.upt.ro>
2020-10-10 22:56:40 +0300

    fix search on mobile

--- src/app/pages/home-page/components/search-bar/search-bar.component.ts
+++ src/app/pages/home-page/components/search-bar/search-bar.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+    selector: 'app-search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.scss']
 })
-export class SearchBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SearchBarComponent {
 }
