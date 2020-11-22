ae0b77f90699cfbb54fee16af58de6629b30d255
Dan Percic <dan.percic@student.upt.ro>
2020-11-22 18:30:48 +0200

    use absolute imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from '@shared/shared.module';
 import { BannerComponent } from './pages/home-page/components/banner/banner.component';
 import { SearchBarComponent } from './pages/home-page/components/search-bar/search-bar.component';
 import { FormsModule } from '@angular/forms';
@@ -26,7 +26,7 @@ import { OfferComponent } from './pages/home-page/components/offer/offer.compone
         HttpClientModule,
         SharedModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
